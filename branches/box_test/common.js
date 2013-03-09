18bacbb90a2814b0382213a6e7496eb763fb41bf
thepotter89@gmail.com <thepotter89@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2013-03-09 11:06:55 +0000

    add box_test

--- branches/box_test/common.js
+++ branches/box_test/common.js
@@ -36,67 +36,10 @@ var histHasArr;
 var histIdStart = idCounter;
 var histIdEnd;
 
-var mainArr; // array for all data.
-var labelArr; // character array for the column names.
 
 
 
 
-function createMainStructure(fileName)
-{
-	var tmpArr = getData(fileName);		
-	var dataArr=tmpArr.dataArr;
-	labelArr=tmpArr.labelArr;	
-	mainArr = make2DArr(labelArr.length);	
-	for(var j=0; j<dataArr.length; j++)
-	{
-		var tmpArr = dataArr[j].toString().split(',');	
-		for(var i = 0 ; i < labelArr.length ; i ++)
-		{
-			mainArr[i][j] = parseFloat(tmpArr[i]);
-		}	
-	}
-}
-function getData(fileName)
-{
-	var filePath = fileName;	
-	xmlhttp = new XMLHttpRequest();	
-	xmlhttp.open("GET",filePath,false);
-	xmlhttp.send(null);
-	var fileContent = xmlhttp.responseText;
-	var tempArr = csv2Arr(fileContent);
-	var returnLabelArr = tempArr[0].toString().split(',');	
-	tempArr.shift();
-	var returnDataArr = tempArr;
-	return { 'dataArr' : returnDataArr, 'labelArr' : returnLabelArr };	
-}
-
-function csv2Arr(data, liveChar)
- {	
- 	var i = 0;
- 	var eof = '';
- 	var cursor = data.charAt(i);
- 	var result_array = new Array();
- 	var result_row = "";
- 	var line = 0;
- 	while(cursor != eof)
- 	{
- 		if((cursor == '\"') || (cursor == '\r') || (cursor == '\t') || (cursor == ' ')){
- 		}else if( cursor == "\n" ){
- 			if (result_array.length <= line)
- 			{
- 				result_array.push(new Array());
- 				result_array[line].push(result_row);
- 				result_row = "";
- 				line++;
- 			}
- 		}else{
- 			result_row += cursor;
- 		}
- 		cursor = data.charAt(i++);
- 	}
- 	return result_array;
- }
 //////////////////////////////////////Chk key event Start//////////////////////////////////////   
 
 window.addEventListener('keydown',this.checkKeyDown,false);	

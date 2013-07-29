8b36c9f93c2a9226b5afab2c8372448a532f5d50
sch8906@gmail.com <sch8906@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2013-07-29 01:30:32 +0000

    Tagging the shiny version of the 'Right' project, before modularizing the code.

--- testFunRIGHT.R
+++ testFunRIGHT.R
@@ -1,15 +1,13 @@
 ## Script to test funRight.R:
 
-source("funRIGHT.R")
+source("testR.R")
 
-sub.diamonds <- diamonds[sample(1:nrow(diamonds), 1000, F), ]
+sub.diamonds <- diamonds[sample(1:nrow(diamonds), 50, F), ]
 
 obj <- RIGHT(sub.diamonds, fun = {
-  plot.RIGHT(sub.diamonds, "x", price, color, "left")
-  plot.RIGHT(sub.diamonds, "clarity", depth, depth, "right")
-  plot.RIGHT(sub.diamonds, "table", cut, cut)
-  hist.RIGHT(sub.diamonds, "color")
-  hist.RIGHT(sub.diamonds, x)
-  hist.RIGHT(sub.diamonds, cut)})
+  plot.RIGHT(sub.diamonds, carat, price, color, "right", 300, 300)
+  hist.RIGHT(sub.diamonds, cut, 2, 300, 300)
+  box.RIGHT(sub.diamonds, carat, price, 300, 300)
+})
 
 print(obj)

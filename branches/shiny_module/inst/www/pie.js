ec71415906a9bce5f52985ea8efb781a8b183aad
sch8906@gmail.com <sch8906@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2013-09-15 15:14:58 +0000

    add pie example.

--- branches/shiny_module/inst/www/pie.js
+++ branches/shiny_module/inst/www/pie.js
@@ -3,13 +3,15 @@ var Pie = {};
 (function() {	
 	Pie = function(axisObj, dataObj, xLabel, yLabel, optionObj) {
 		this._init(axisObj, dataObj, optionObj);
-		this._checkStacking(axisObj, dataObj, xLabel, yLabel);
+		//this._checkStacking(axisObj, dataObj, xLabel, yLabel);
 		this._draw(axisObj, dataObj, xLabel, yLabel);
+		axisObj.numberOfGraph ++;
 		dataObj.$id ++;
 	};
 	Pie.prototype = {
 			_init: function(axisObj, dataObj, optionObj) {
-				this.id = dataObj.$id;
+				this.dataId = dataObj.$id;
+				this.graphId = axisObj.numberOfGraph;
 				this.barWidth = axisObj.xbarWidth;
 				// check color
 				if(axisObj.legendLabel != undefined){
@@ -60,7 +62,6 @@ var Pie = {};
 			},
 			_draw: function(axisObj, dataObj, xLabel, yLabel) {
 				this.node = new Array();
-				var cnt = 0;
 				var degree = 0;
 				// calculate total frequency.
 				var totalFreq = 0;
@@ -77,14 +78,15 @@ var Pie = {};
 	    					radius: 100,
 	    					rotationDeg: -90 + degree,	
 	    					angleDeg: dataObj[yLabel][i]/totalFreq * 360,
-	    					fill: (dataObj[this.colorLabel].isDiscrete == undefined) ? dataObj[this.colorLabel].color[i] : dataObj[this.colorLabel].colorIndex[dataObj[this.colorLabel][i]],
 	    					stroke: (dataObj[this.colorLabel].isDiscrete == undefined) ? dataObj[this.colorLabel].color[i] : dataObj[this.colorLabel].colorIndex[dataObj[this.colorLabel][i]],
+	    					fill: (dataObj[this.colorLabel].isDiscrete == undefined) ? dataObj[this.colorLabel].color[i] : dataObj[this.colorLabel].colorIndex[dataObj[this.colorLabel][i]],
 	    					strokeWidth: 0.2,
 	    					opacity : 0.5,
 	    					selected : 0,
 	    					selectCnt : 0,
 	    					info: "Node: " + i + "\r\n" + getNodeinfo(dataObj, i)
 	    				});
+						dataObj.$isSelected[i][this.dataId] = pieUpdate(this.node[i]);
 	        			degree = degree + dataObj[yLabel][i]/totalFreq * 360;
 					}
 				}else{
@@ -105,20 +107,89 @@ var Pie = {};
 	    					selectCnt : 0,
 	    					info: "Node: " + i + "\r\n" + getNodeinfo(dataObj, i)
 	    				});
+						dataObj.$isSelected[i][this.dataId] = pieUpdate(this.node[i]);
 	        			degree = degree + dataObj[yLabel][i]/totalFreq * 360;
 					}
 				}
+				// event add
+				dataObj.refreshArr[this.dataId] = makeRefresh(axisObj.stage);
+				this.firstUpdate = firstUpdate(dataObj);
+				this.dataObj = dataObj;
 				this.dataLayer = new Kinetic.Layer();	
 	        	for(var i = 0 ; i < this.node.length ; i ++){
 					this.dataLayer.add(this.node[i]);
 				}
-				axisObj.dataLayerArr.push(this.dataLayer);
-				axisObj.hoverArr.push(pieHover());
+	        	axisObj.graphObjArr[this.graphId] = this;
+	        	axisObj.dataLayerArr[this.graphId] = this.dataLayer;
+				axisObj.hoverArr[this.graphId] = pieHover();
+				axisObj.boxSearchArr[this.graphId] = pieBoxSearch(this);
 				//add layer
 				axisObj.stage.add(this.dataLayer);
 			}
 	};
 })();
+
+function pieBoxSearch(graphObj)
+{
+	return function(smallX, smallY, bigX, bigY)
+		{	
+			var tmpNodeArr = new Array();
+			var tmpNodeArr1 = new Array();
+			if(ctrlPressed == true) {
+				for(var i = 0 ; i < graphObj.node.length ; i ++){
+					if((smallX <= graphObj.node[i].getX() + graphObj.node[i].getWidth() && graphObj.node[i].getX() <= bigX) && (smallY <= graphObj.node[i].getY() + graphObj.node[i].getHeight() && graphObj.node[i].getY() <= bigY)){
+						if(graphObj.node[i].getSelected() == 1){
+							tmpNodeArr.push(graphObj.node[i].getName());
+						}else{
+							tmpNodeArr1.push(graphObj.node[i].getName());
+						}        
+					}
+				}
+			//	alert("ddd");
+				if(tmpNodeArr.length != undefined){
+					allGraphUpdate(graphObj, tmpNodeArr, 0);
+				}
+				if(tmpNodeArr1.length != undefined){
+					allGraphUpdate(graphObj, tmpNodeArr1, 1);
+				}				
+			}else{
+				for(var i = 0 ; i < graphObj.node.length ; i ++){
+					if((smallX <= graphObj.node[i].getX() + graphObj.node[i].getWidth() && graphObj.node[i].getX() <= bigX) && (smallY <= graphObj.node[i].getY() + graphObj.node[i].getHeight() && graphObj.node[i].getY() <= bigY)){
+						tmpNodeArr.push(graphObj.node[i].getName());
+	                }
+	           	}
+				allGraphUpdate(graphObj, tmpNodeArr, 1);
+	        }
+			
+		};
+}
+/**  update function  **/
+//Kinetic version update
+//just remove transitient, and change it with "set" syntax.
+//"set" syntax has not changed during many versions.
+function pieUpdate(node)
+{
+	return	function(selectOn)
+		{
+			if(node.getSelected() == 1 && selectOn == 0){		//unselect
+				node.setSelectCnt(node.getSelectCnt() - 1);	
+				if(node.getSelectCnt() == 0){
+					node.setOpacity(0.5);
+					node.setStroke(node.getFill());
+					node.setSelected(0);
+				}
+			}else if(selectOn == 1){		// select
+				node.setSelectCnt(node.getSelectCnt() + 1);
+				if(node.getSelected() == 0){
+					node.setStroke('black');
+					node.setOpacity(1);
+					node.setSelected(1);
+				}
+			}
+			
+		};
+}
+/**  update function end  **/
 function pieHover()
 {
 	return function(node, overOff) // over: 1 , off: 0
@@ -135,46 +206,3 @@ function pieHover()
 			}			
 		};
 }
-/**  update function  **/
-//Kinetic version update
-//just remove transitient, and change it with "set" syntax.
-//"set" syntax has not changed during many versions.
-function pieUpdate(node)
-{
-	return	function(ids, selectOn)
-				{
-					if(ids.length == undefined){
-						if(node[ids].getSelected() == 1 && selectOn == 0){		//unselect
-							node[ids].setSelectCnt(obj.node[ids].getSelectCnt() - 1);
-							if(node[ids].getSelectCnt() == 0){
-								node[ids].setOpacity(0.5);
-								node[ids].setSelected(0);
-							}
-						}else if(selectOn == 1){		// select
-							if(node[ids].getSelected() == 0){
-								node[ids].setOpacity(1);
-								node[ids].setSelected(1);
-							}
-							node[ids].setSelectCnt(node[ids].getSelectCnt() + 1);
-						}
-					}else{
-						for(var i = 0 ; i < ids.length ; i ++){
-							if(node[ids[i]].getSelected() == 1 && selectOn == 0){		//unselect
-								node[ids[i]].setSelectCnt(obj.node[ids[i]].getSelectCnt() - 1);
-								if(node[ids[i]].getSelectCnt() == 0){
-									node[ids[i]].setOpacity(0.5);
-									node[ids[i]].setSelected(0);
-								}
-							}else if(selectOn == 1){		// select
-								if(node[ids[i]].getSelected() == 0){
-									node[ids[i]].setOpacity(1);
-									node[ids[i]].setSelected(1);
-								}
-								node[ids[i]].setSelectCnt(node[ids[i]].getSelectCnt() + 1);
-							}
-						}
-					}
-					
-				};
-}
-/**  update function end  **/

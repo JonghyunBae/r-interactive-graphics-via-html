e2cc9c9795bbffec5063b0c7dabf7a622194bd4f
flammy@gmail.com <flammy@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2013-09-23 02:57:03 +0000

    Changed "n" to NULL to represent missing, and examples for RIGHT and boxplot are updated.

--- trunk/shiny_module/inst/tests/testPoints_RIGHT.R
+++ trunk/shiny_module/inst/tests/testPoints_RIGHT.R
@@ -44,14 +44,14 @@ test_that("Check script generation", {
   points_RIGHT(conc ~ Time, Theoph)
   temp <- get(".RIGHT", envir = asNamespace("RIGHT"))
   expect_identical(temp$numPoints, 1)
-  expect_identical(temp$scriptArray, "var point1 = new Dot(axis1, Theoph, 'Time', 'conc', {baseColor: 'black'});")
+  expect_identical(temp$scriptArray, "var point1 = new Dot(axis1, Theoph, 'Time', 'conc', {});")
   expect_true(any(file.path(temp$libDir_RIGHT, "dot.js") %in% temp$sourceArray))
   
   points_RIGHT(conc ~ Time, "Theoph")
   temp <- get(".RIGHT", envir = asNamespace("RIGHT"))
   expect_identical(temp$numPoints, 2)
-  expect_identical(temp$scriptArray, c("var point1 = new Dot(axis1, Theoph, 'Time', 'conc', {baseColor: 'black'});",
-                                       "var point2 = new Dot(axis1, Theoph, 'Time', 'conc', {baseColor: 'black'});"))
+  expect_identical(temp$scriptArray, c("var point1 = new Dot(axis1, Theoph, 'Time', 'conc', {});",
+                                       "var point2 = new Dot(axis1, Theoph, 'Time', 'conc', {});"))
   expect_true(any(file.path(temp$libDir_RIGHT, "dot.js") %in% temp$sourceArray))
   
 }) # test_that

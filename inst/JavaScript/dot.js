6f910e354dded074df615fcafd6ff36fb27c2b87
sch8906 <sch8906@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2014-05-01 21:23:46 +0000

    fix ctrl bug

--- inst/JavaScript/dot.js
+++ inst/JavaScript/dot.js
@@ -127,22 +127,17 @@ function dotBoxSearch (graphObj) {
 			if (ctrlPressed == true) {
 				for (var i=0; i<graphObj.node.length; i++) {
 					if (smallX <= graphObj.node[i].getX() && graphObj.node[i].getX() <= bigX && smallY <= graphObj.node[i].getY() && graphObj.node[i].getY() <= bigY) {
-						if (graphObj.node[i].getSelected()==1) {
-							tmpNodeArr.push(graphObj.node[i].getName());
-						} else {
-							tmpNodeArr1.push(graphObj.node[i].getName());
-						}
+						tmpNodeArr.push(graphObj.node[i].getName());
 	                }
 				}
-				allGraphUpdate(graphObj, tmpNodeArr, 0);
-				allGraphUpdate(graphObj, tmpNodeArr1, 1);
+				allGraphUpdate(graphObj, tmpNodeArr, null, 'ctrl');
 			} else {
 				for (var i=0; i<graphObj.node.length; i++) {
 					if (smallX <= graphObj.node[i].getX() && graphObj.node[i].getX() <= bigX && smallY <= graphObj.node[i].getY() && graphObj.node[i].getY() <= bigY) {
 						tmpNodeArr.push(graphObj.node[i].getName());
 	                }
 				}
-				allGraphUpdate(graphObj, tmpNodeArr, 1);
+				allGraphUpdate(graphObj, tmpNodeArr, 1, null);
 			}
 		};
 }

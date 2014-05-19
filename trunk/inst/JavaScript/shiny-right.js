86daeb92bca7dfcbf6259d8f3f197ff0127fcbdd
sch8906@gmail.com <sch8906@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2014-05-19 21:34:42 +0000

    Checking whether modification can be applied to offload code or not.
    Can be applied!

--- trunk/inst/JavaScript/shiny-right.js
+++ trunk/inst/JavaScript/shiny-right.js
@@ -7,15 +7,12 @@ rightOutputBinding.find = function(scope){
 rightOutputBinding.renderValue = function (el, data) {
 	if (cnt > 0) {//if(data change)
 		cnt=0;
-		if (data[0] != -1) {			
+		if (data[0] != -1) {
+			// TODO: Shoul we apply this method? xx and yy ?
 			for (var i=0; i<data.length; i=i+2) {
 				window[data[i]].xx = data[i+1].xx;
 				window[data[i]].yy = data[i+1].yy;
 				window[data[i]].draw();
-				for(var j = 0 ; j < window[data[i]].$isSelected.length ; j ++){
-					//window[data[i]].$isSelected[j](data[i+1]);
-				}
-				//window.Shiny.onInputChange(data[i].relateObjName, -1);
 			}			
 		}
 	}

931d40035efde44f1bf2a5359a31e234cacefaa3
sch8906@gmail.com <sch8906@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2014-05-20 08:40:23 +0000

    Update server.R and index.html of serveroffload code. using multiple <div>s to make communication of multiple offloadObjs parallel.
    It works.

--- inst/examples/example_offload/server.R
+++ inst/examples/example_offload/server.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  output$content <- reactive({
+  output$content1 <- reactive({
     if (length(input$mainArr1) != 0) {
      if (length(input$mainArr1) > 1) {
        x <- Theoph$Time[!input$mainArr1]
@@ -12,12 +12,32 @@ shinyServer(function(input, output) {
        xRange <- range(x)
        xArray <- seq(xRange[1], xRange[2], length.out = length(x))
        yArray <- predict(obj, data.frame(x = xArray))
-       output <- list("regArr", data.frame(xx = xArray, yy = yArray))
+       output <- list("regArr1", data.frame(xx = xArray, yy = yArray))
        return(output)
      } else {
        output <- list(-1, -1)
        return (output)
      }
+    }
+  })
+  
+  output$content2 <- reactive({
+    if (length(input$mainArr2) != 0) {
+      if (length(input$mainArr2) > 1) {
+        x <- Theoph$Time[!input$mainArr2]
+        y <- Theoph$conc[!input$mainArr2]
+        p <- cbind(x, y)
+        p <- data.frame(p)
+        obj <- lm(y ~ x, p)
+        xRange <- range(x)
+        xArray <- seq(xRange[1], xRange[2], length.out = length(x))
+        yArray <- predict(obj, data.frame(x = xArray))
+        output <- list("regArr2", data.frame(xx = xArray, yy = yArray))
+        return(output)
+      } else {
+        output <- list(-1, -1)
+        return (output)
+      }
     }
   })
 })

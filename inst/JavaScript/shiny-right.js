74e32ac13f2715afcbed326fa301995f707e7929
bnbbkr@gmail.com <bnbbkr@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2014-06-07 02:20:16 +0000

    # complete to make server-offloading example locally

--- inst/JavaScript/shiny-right.js
+++ inst/JavaScript/shiny-right.js
@@ -9,8 +9,8 @@ rightOutputBinding.renderValue = function (el, data) {
 		if (data != null) {
 			if (data[0] != -1) {
 				// TODO: Shoul we apply this method? xx and yy ?
-				for (var name in window[data[0]]) {
-					window[data[0]][name] = data[1][name];
+				for (var name in data[1]) {
+  				window[data[0]][name] = data[1][name];
 				}
 				window[data[0]].draw();
 			}

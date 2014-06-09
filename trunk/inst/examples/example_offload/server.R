f178b07d35d9dc891cb90df868629293ab823e95
bnbbkr@gmail.com <bnbbkr@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2014-06-09 05:45:42 +0000

    # Add second data object (lmArray) in server-offloading

--- trunk/inst/examples/example_offload/server.R
+++ trunk/inst/examples/example_offload/server.R
@@ -2,18 +2,18 @@ library(shiny)
 
 shinyServer(function(input, output) {
   output$loessArray <- reactive({
-    if (length(input$mainArr1) != 0) {
-      if (length(input$mainArr1) > 1) {
+    if (length(input$subDiamonds) != 0) {
+      if (length(input$subDiamonds) > 1) {
        
         # just copy
-        obj <- loess(Time ~ conc, data = Theoph)
-        xRange <- range(Theoph$conc)
-        simArray <- data.frame(conc = seq(xRange[1], xRange[2], length.out = 132))
-        simArray$Time <- predict(obj, newdata = simArray)
+        obj <- loess(price ~ carat, data = subDiamonds)
+        xRange <- range(subDiamonds$carat)
+        simArray <- data.frame(carat = seq(xRange[1], xRange[2], length.out = 100))
+        simArray$price <- predict(obj, newdata = simArray)
        
         # generation
-        temp.x <- simArray[[colnames(simArray)[1]]][!input$mainArr1]
-        temp.y <- simArray[[colnames(simArray)[2]]][!input$mainArr1]
+        temp.x <- simArray[[colnames(simArray)[1]]][!input$subDiamonds]
+        temp.y <- simArray[[colnames(simArray)[2]]][!input$subDiamonds]
         
         temp.names <- colnames(simArray)
         simArray <- data.frame(temp.x, temp.y)
@@ -24,6 +24,36 @@ shinyServer(function(input, output) {
         # can't multiple output in R
         return(list("loessArray", simArray)) # output$loessArray <- simArray
        
+      } else {
+        output <- list(-1, -1)
+        return (output)
+      }
+    }
+  })
+  
+  output$lmArray <- reactive({
+    if (length(input$Theoph) != 0) {
+      if (length(input$Theoph) > 1) {
+        
+        # just copy
+        obj <- lm(conc ~ Time, data = Theoph)
+        xRange <- range(Theoph$Time)
+        simArray <- data.frame(Time = seq(xRange[1], xRange[2], length.out = 132))
+        simArray$conc <- predict(obj, newdata = simArray)
+        
+        # Need to change new method
+        temp.x <- simArray[[colnames(simArray)[1]]][!input$Theoph]
+        temp.y <- simArray[[colnames(simArray)[2]]][!input$Theoph]
+        
+        temp.names <- colnames(simArray)
+        simArray <- data.frame(temp.x, temp.y)
+        
+        for(i in 1:length(simArray))
+          colnames(simArray)[i] <- temp.names[i]
+        
+        # can't multiple output in R
+        return(list("lmArray", simArray)) # output$loessArray <- simArray
+        
       } else {
         output <- list(-1, -1)
         return (output)

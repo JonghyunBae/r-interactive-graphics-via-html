4b8936bbf93bc44e961140c261c664b1b20bab9e
flammy@gmail.com <flammy@gmail.com@6f6577d9-8cc9-916a-33ed-22dfac4d7a31>
2014-06-30 23:12:56 +0000

    Added rjson library in the DESCRIPTION file.

--- trunk/R/body.R
+++ trunk/R/body.R
@@ -30,7 +30,7 @@ prepareData <- function(dataList, dir = ".") {
     tempData <- as.list(dataList[[nameArray[iData]]])
     dataArr <- lapply(tempData, function(x) if (is.factor(x)) list(level = levels(x), index = as.numeric(x) - 1) else x)
     
-    mainArr <- toJSON(dataArr)
+    mainArr <- rjson::toJSON(dataArr)
     
     dataScript <- c(dataScript, paste0("var ", nameArray[iData], " = ", mainArr, ";"))
   } # for
